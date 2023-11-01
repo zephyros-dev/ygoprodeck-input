57325bfc5f082aaa83012268064aa664e5ab2ef9
ptn <ptn@cct-technology.com>
2023-11-01 09:22:06 +0000

    feat: add null row prune

---
+++ README.md
@@ -0,0 +1,14 @@
+# Usage
+
+1. Prepare config.ini for configuration
+2. Prepare `collection.csv` from ygoprodeck
+3. Prepare missing.txt file for missing card, separated by space
+4. Prepare available.txt for available card, separated by space
+5. Run the script
+
+```
+./main.py
+```
+
+5. Check the result in terminal
+6. Upload the `output.csv` file to ygoprodeck
--- main.py
+++ main.py
@@ -4,25 +4,33 @@ import configparser
 import csv
 import pandas as pd
 from pathlib import Path
+import re
 
 config = configparser.ConfigParser()
 
 config.read("default.ini")
-config.read("config.ini")
+if Path("config.ini").is_file():
+    config.read("config.ini")
 
 MISSING_CARD_QUANTITY = 99
 
 df = pd.read_csv(config.get("global", "collection_path")).sort_values(by=["cardcode"])
+df.dropna(inplace=True)
+df = df.astype({"cardq": int, "cardid": int})
 missing_cards_string = open("missing.txt").read().replace("\n", " ").split()
 missing_cards_list = "|".join([f"{int(x):03d}" for x in missing_cards_string])
 available_card_string = open("available.txt").read().replace("\n", " ").split()
-available_card_list= "|".join([f"{int(x):03d}" for x in available_card_string])
+available_card_list = "|".join([f"{int(x):03d}" for x in available_card_string])
 if missing_cards_list:
-    df.loc[df["cardcode"].str.contains(missing_cards_list), "cardq"] = MISSING_CARD_QUANTITY
+    df.loc[
+        df["cardcode"].str.contains(missing_cards_list), "cardq"
+    ] = MISSING_CARD_QUANTITY
     print(df[df["cardq"] == MISSING_CARD_QUANTITY])
 if available_card_list:
     df["cardq"] = MISSING_CARD_QUANTITY
     df.loc[df["cardcode"].str.contains(available_card_list), "cardq"] = 1
     print(df[df["cardq"] != MISSING_CARD_QUANTITY])
 df.to_csv("output.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
-Path(config.get("global", "collection_path")).rename(f'{config.get("global", "collection_path")}_old')
+Path(config.get("global", "collection_path")).rename(
+    re.sub(".csv", "_old.csv", config.get("global", "collection_path"))
+)
